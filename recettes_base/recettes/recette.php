8b89a8887e6298b160706670c47afb89d3ed4c60
campeurman <campeurman@hotmail.fr>
2020-03-23 16:34:32 +0100

    rectif fin d'une recette

--- recettes_base/recettes/recette.php
+++ recettes_base/recettes/recette.php
@@ -1,6 +1,6 @@
 
         <?php
-        $recette [
+        $recette =[
             "titre"=>"Entree 1",
             "image"=>"img/entree1.jpg",
             "temp"=>"15sec",
@@ -8,24 +8,22 @@
             "difficulte"=>"1/4",
             "cout"=>"1/4",
             "ingredients"=>"1 paquet de pâtes de lasagnes///3 oignons jaunes///3 gousses d'ail///1 branche de céleri///1 carotte",
-            "etapes"=>"Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout."///Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation.,
+            "etapes"=>"Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout.///Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation."
             ];
            
 $length=sizeof($recette);
 
-
-
-    $titre=$recette"[titre]";
-    $image = $recette"[image]";
-    $temp = $recette"[temp]";
-    $personne = $recette"[personne]";
-    $difficulte = $recette"[difficulte]";
-    $cout = $recette"[cout]";
-    $ingrediants = $recette"[ingredients]";
-    $etapes =$recette"[etapes]";
-
-    $inter = explode("///",$ingredients);
-    $inter2 = explode("///",$etapes);
+    $titre = $recette["titre"];
+    $image = $recette["image"];
+    $temp = $recette["temp"];
+    $personne = $recette["personne"];
+    $difficulte = $recette["difficulte"];
+    $cout = $recette["cout"];
+    $ingredients = $recette["ingredients"];
+    $etapes = $recette["etapes"];
+    
+    $inter = explode("///", $ingredients);
+    $inter2 = explode("///", $etapes);
     ?>
 <h1 class="recipe-head\"><?=$titre?></h1><div class="illustration img-block"><img src=\"<?=$image?>\" alt=""></div><section class="row bg-light">
 <div class="col-6">
@@ -45,12 +43,12 @@ $length=sizeof($recette);
                 </table>
 
                 <h2>Ingrédients</h2>
-                <ul>"      
+                <ul>      
                     <li><?=$inter[0]?></li>
-                    <li><?=inter[1]?></li>
-                    <li><?=inter[2]?></li>
-                    <li><?=inter[3]?></li>
-                    <li><?=inter[4]?></li>
+                    <li><?=$inter[1]?></li>
+                    <li><?=$inter[2]?></li>
+                    <li><?=$inter[3]?></li>
+                    <li><?=$inter[4]?></li>
                     
                 </ul>
             </div>
